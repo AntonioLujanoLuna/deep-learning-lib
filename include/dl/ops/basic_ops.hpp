f961d4c544bad41bc4692573b1fd76ad24c24699
Antonio Lujano Luna <a00lujano@gmail.com>
2025-01-14 18:15:40 +0100

    Header-only project

--- include/dl/ops/basic_ops.hpp
+++ include/dl/ops/basic_ops.hpp
@@ -1,43 +1,27 @@
 #pragma once
 
-#include "../tensor.hpp"
 #include "../autograd.hpp"
-#include "../detail/tensor_impl.hpp"
 #include <memory>
 #include <vector>
+#include <string>
 
 namespace dl {
+
+// Forward declarations
+template<typename T> class Tensor;
+
+namespace detail {
+template<typename T> class TensorImpl;
+}
+
 namespace ops {
 
 template<typename T>
 class AddNode : public Node {
 public:
-    AddNode(const Tensor<T>& a, const Tensor<T>& b, Tensor<T>& output)
-        : a_impl_(a.impl_)
-        , b_impl_(b.impl_)
-        , output_impl_(output.impl_) {}
-
-    std::string node_type() const override {
-        return "Add";
-    }
-
-    void backward() override {
-        const auto& out_grad = output_impl_->grad();
-        
-        if (a_impl_->requires_grad()) {
-            auto& a_grad = a_impl_->grad();
-            for (size_t i = 0; i < out_grad.size(); ++i) {
-                a_grad[i] += out_grad[i];  // Add gradient contribution
-            }
-        }
-        
-        if (b_impl_->requires_grad()) {
-            auto& b_grad = b_impl_->grad();
-            for (size_t i = 0; i < out_grad.size(); ++i) {
-                b_grad[i] += out_grad[i];  // Add gradient contribution
-            }
-        }
-    }
+    AddNode(const Tensor<T>& a, const Tensor<T>& b, Tensor<T>& output);
+    std::string node_type() const override { return "Add"; }
+    void backward() override;
 
 private:
     std::shared_ptr<detail::TensorImpl<T>> a_impl_;
@@ -48,40 +32,68 @@ private:
 template<typename T>
 class MulNode : public Node {
 public:
-    MulNode(const Tensor<T>& a, const Tensor<T>& b, Tensor<T>& output)
-        : a_impl_(a.impl_)
-        , b_impl_(b.impl_)
-        , output_impl_(output.impl_) {}
-
-    std::string node_type() const override {
-        return "Multiply";
-    }
-
-    void backward() override {
-        const auto& out_grad = output_impl_->grad();
-        const auto& a_data = a_impl_->data();
-        const auto& b_data = b_impl_->data();
-        
-        if (a_impl_->requires_grad()) {
-            auto& a_grad = a_impl_->grad();
-            for (size_t i = 0; i < out_grad.size(); ++i) {
-                a_grad[i] += out_grad[i] * b_data[i];  // Chain rule: dy/da = b * upstream_grad
-            }
-        }
-        
-        if (b_impl_->requires_grad()) {
-            auto& b_grad = b_impl_->grad();
-            for (size_t i = 0; i < out_grad.size(); ++i) {
-                b_grad[i] += out_grad[i] * a_data[i];  // Chain rule: dy/db = a * upstream_grad
-            }
-        }
-    }
+    MulNode(const Tensor<T>& a, const Tensor<T>& b, Tensor<T>& output);
+    std::string node_type() const override { return "Multiply"; }
+    void backward() override;
 
 private:
     std::shared_ptr<detail::TensorImpl<T>> a_impl_;
     std::shared_ptr<detail::TensorImpl<T>> b_impl_;
     std::shared_ptr<detail::TensorImpl<T>> output_impl_;
 };
+
+// Template implementations
+template<typename T>
+AddNode<T>::AddNode(const Tensor<T>& a, const Tensor<T>& b, Tensor<T>& output)
+    : a_impl_(a.impl_)
+    , b_impl_(b.impl_)
+    , output_impl_(output.impl_) {}
+
+template<typename T>
+void AddNode<T>::backward() {
+    const auto& out_grad = output_impl_->grad();
+    
+    if (a_impl_->requires_grad()) {
+        auto& a_grad = a_impl_->grad();
+        for (size_t i = 0; i < out_grad.size(); ++i) {
+            a_grad[i] += out_grad[i];
+        }
+    }
+    
+    if (b_impl_->requires_grad()) {
+        auto& b_grad = b_impl_->grad();
+        for (size_t i = 0; i < out_grad.size(); ++i) {
+            b_grad[i] += out_grad[i];
+        }
+    }
+}
+
+template<typename T>
+MulNode<T>::MulNode(const Tensor<T>& a, const Tensor<T>& b, Tensor<T>& output)
+    : a_impl_(a.impl_)
+    , b_impl_(b.impl_)
+    , output_impl_(output.impl_) {}
+
+template<typename T>
+void MulNode<T>::backward() {
+    const auto& out_grad = output_impl_->grad();
+    const auto& a_data = a_impl_->data();
+    const auto& b_data = b_impl_->data();
+    
+    if (a_impl_->requires_grad()) {
+        auto& a_grad = a_impl_->grad();
+        for (size_t i = 0; i < out_grad.size(); ++i) {
+            a_grad[i] += out_grad[i] * b_data[i];
+        }
+    }
+    
+    if (b_impl_->requires_grad()) {
+        auto& b_grad = b_impl_->grad();
+        for (size_t i = 0; i < out_grad.size(); ++i) {
+            b_grad[i] += out_grad[i] * a_data[i];
+        }
+    }
+}
 
 } // namespace ops
 } // namespace dl
--- include/dl/tensor.hpp
+++ include/dl/tensor.hpp
@@ -6,9 +6,8 @@
 #include <stdexcept>
 #include <iostream>
 
-namespace dl {
-
 // Forward declarations
+namespace dl {
 namespace ops {
 template<typename T> class AddNode;
 template<typename T> class MulNode;
@@ -46,14 +45,14 @@ public:
     void set_requires_grad(bool requires_grad) { impl_->set_requires_grad(requires_grad); }
     void zero_grad() { impl_->zero_grad(); }
 
-    // Operators
+    // Forward declarations of operators
     Tensor<T> operator+(const Tensor<T>& other) const;
     Tensor<T> operator*(const Tensor<T>& other) const;
 
 private:
     TensorPtr impl_;
 
-    // Allow nodes to access impl_
+    // Give access to operation nodes
     template<typename U> friend class ops::AddNode;
     template<typename U> friend class ops::MulNode;
     template<typename U> friend class ops::MatMulNode;
@@ -62,5 +61,80 @@ private:
     template<typename U> friend class ops::TanhNode;
     template<typename U> friend class nn::LinearNode;
 };
+
+} // namespace dl
+
+// Include operator implementations after Tensor class definition
+#include "ops/basic_ops.hpp"
+#include "ops/broadcast.hpp"
+#include "dl/autograd.hpp"
+
+namespace dl {
+
+template<typename T>
+Tensor<T> Tensor<T>::operator+(const Tensor<T>& other) const {
+    if (!impl_ || !other.impl_) {
+        throw std::runtime_error("Tensor implementation is null");
+    }
+
+    auto broadcast_shape = ops::compute_broadcast_shape(shape(), other.shape());
+    
+    // Broadcast inputs if necessary
+    std::vector<T> a_data = (shape() == broadcast_shape) ? 
+        data() : ops::broadcast_to(data(), shape(), broadcast_shape);
+    std::vector<T> b_data = (other.shape() == broadcast_shape) ? 
+        other.data() : ops::broadcast_to(other.data(), other.shape(), broadcast_shape);
+    
+    Tensor<T> result(broadcast_shape);
+    auto& result_data = result.data();
+    
+    for (size_t i = 0; i < result_data.size(); ++i) {
+        result_data[i] = a_data[i] + b_data[i];
+    }
+    
+    // Set requires_grad and create backward node if either input requires grad
+    if (requires_grad() || other.requires_grad()) {
+        result.set_requires_grad(true);
+        auto node = std::make_shared<ops::AddNode<T>>(*this, other, result);
+        ComputationGraph::getInstance().addNode(node);
+    }
+    
+    return result;
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::operator*(const Tensor<T>& other) const {
+    if (!impl_ || !other.impl_) {
+        throw std::runtime_error("Tensor implementation is null");
+    }
+
+    auto broadcast_shape = ops::compute_broadcast_shape(shape(), other.shape());
+    
+    // Broadcast inputs if necessary
+    std::vector<T> a_data = (shape() == broadcast_shape) ? 
+        data() : ops::broadcast_to(data(), shape(), broadcast_shape);
+    std::vector<T> b_data = (other.shape() == broadcast_shape) ? 
+        other.data() : ops::broadcast_to(other.data(), other.shape(), broadcast_shape);
+    
+    Tensor<T> result(broadcast_shape);
+    auto& result_data = result.data();
+    
+    for (size_t i = 0; i < result_data.size(); ++i) {
+        result_data[i] = a_data[i] * b_data[i];
+    }
+    
+    // Set requires_grad and create backward node if either input requires grad
+    if (requires_grad() || other.requires_grad()) {
+        result.set_requires_grad(true);
+        auto node = std::make_shared<ops::MulNode<T>>(*this, other, result);
+        ComputationGraph::getInstance().addNode(node);
+    }
+    
+    return result;
+}
+
+// Template instantiations for commonly used types
+template class Tensor<float>;
+template class Tensor<double>;
 
 } // namespace dl
